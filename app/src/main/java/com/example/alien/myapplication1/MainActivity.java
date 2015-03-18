bc8813a2e628e3fdbc90b257b156d884d54bff43
RedneckZombie <ciartur@wp.pl>
2015-03-18 14:14:01 +0100

    EE

---
+++ app/src/main/java/com/example/alien/myapplication1/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.alien.myapplication1;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    private EditText login_mail;
+    private EditText login_password;
+    private CheckBox checkbox_remember;
+    private Button button_login;
+    private Button button_guest;
+    private Button button_register;
+    SharedPreferences preferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        login_mail = (EditText) findViewById(R.id.email);
+        login_password = (EditText) findViewById(R.id.password);
+        checkbox_remember = (CheckBox) findViewById(R.id.checkBox_remember);
+        button_login = (Button) findViewById(R.id.Login);
+        button_guest = (Button) findViewById(R.id.guest);
+        button_register = (Button) findViewById(R.id.registration_button);
+        preferences = getSharedPreferences("PREFS", Activity.MODE_PRIVATE);
+        addListeners();
+        restoreData();
+    }
+
+    public void restoreData()
+    {
+        login_mail.setText(preferences.getString("mail", ""));
+        login_password.setText(preferences.getString("pass", ""));
+        checkbox_remember.setChecked(preferences.getBoolean("rem", false));
+    }
+    public void addListeners()
+    {
+        button_guest.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(getApplicationContext(), MapActivity.class);
+                startActivity(intent);
+            }
+        });
+        button_register.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(getApplicationContext(), RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
+        button_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor pref = preferences.edit();
+                if(checkbox_remember.isChecked())
+                {
+                    pref.putString("mail", login_mail.getText().toString());
+                    pref.putString("pass", login_password.getText().toString());
+                    pref.putBoolean("rem", checkbox_remember.isChecked());
+                }
+                else{
+                    pref.putString("mail", "");
+                    pref.putString("pass", "");
+                    pref.putBoolean("rem", false);
+                }
+                pref.commit();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    public void login(View view){
+        //String username = usernameField.getText().toString();
+        //String password = passwordField.getText().toString();
+        //method.setText("Get Method");
+        //new SigninActivity(this,status,role,0).execute(username,password);
+
+    }
+    public void loginPost(View view){
+        //String username = usernameField.getText().toString();
+        //String password = passwordField.getText().toString();
+        //method.setText("Post Method");
+        //new SigninActivity(this,status,role,1).execute(username,password);
+
+    }
+
+}
---
+++ app/src/main/java/com/example/alien/myapplication1/RegistrationActivity.java
@@ -0,0 +1,102 @@
+package com.example.alien.myapplication1;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+
+public class RegistrationActivity extends ActionBarActivity {
+
+    private EditText etEmail, etPassword, etPasswordConfirm, etUserName, etDateOfBirth, etCity;
+    private RadioButton female, male;
+    private Button saveSignUp, clear;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        etPasswordConfirm = (EditText) findViewById(R.id.etPasswordConfirm);
+        etUserName = (EditText) findViewById(R.id.etUserName);
+        etDateOfBirth = (EditText) findViewById(R.id.etDateOfBirth);
+        etCity = (EditText) findViewById(R.id.etCity);
+
+        female = (RadioButton) findViewById(R.id.female);
+        male = (RadioButton) findViewById(R.id.male);
+
+        saveSignUp = (Button) findViewById(R.id.saveSignUp);
+        clear = (Button) findViewById(R.id.clear);
+
+        listener();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_registration, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void listener()
+    {
+        saveSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(etPassword.getText().toString().equals(etPasswordConfirm.getText().toString()))
+                {
+                    String email = etEmail.getText().toString();
+                    String password = etPassword.getText().toString();
+                    String user_name = etUserName.getText().toString();
+                    String dateOfBirth = etDateOfBirth.getText().toString();
+                    String city = etCity.getText().toString();
+
+                    new SigninActivity(getApplicationContext(),1).execute(email,password, user_name);
+
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"Hasło i potwierdzenie hasła nie są takie same !", Toast.LENGTH_SHORT).show();
+                    //WYWALIĆ POWIADOMINEIE
+                    etPassword.setText("");
+                    etPasswordConfirm.setText("");
+                }
+            }
+        });
+
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                etEmail.setText("");
+                etPassword.setText("");
+                etPasswordConfirm.setText("");
+                etUserName.setText("");
+                etDateOfBirth.setText("");
+                etCity.setText("");
+            }
+        });
+    }
+}
