62a6de721130f7b16119464863fd20e07f7eebd3
RedneckZombie <ciartur@wp.pl>
2015-04-29 19:55:59 +0200

    ....

---
+++ app/src/main/java/com/example/alien/myapplication1/tracks/GetTracks.java
@@ -0,0 +1,76 @@
+package com.example.alien.myapplication1.tracks;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Created by Alien on 2015-04-29.
+ */
+public class GetTracks extends AsyncTask<String,Void,String> {
+    private Context context;
+
+    public GetTracks(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    protected String doInBackground(String... arg0) {
+        try{
+            String account_id = (String)arg0[0];
+            String link = "http://rommam.cba.pl/get_tracks.php";
+            String data  = "account_id"
+                    + "=" + account_id;
+            URL url = new URL(link);
+            URLConnection conn = url.openConnection();
+            conn.setDoOutput(true);
+            OutputStreamWriter wr = new OutputStreamWriter
+                    (conn.getOutputStream());
+            wr.write( data );
+            wr.flush();
+
+            InputStreamReader isr = new InputStreamReader(conn.getInputStream());
+            BufferedReader reader = new BufferedReader(isr);
+
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            // Read Server Response
+            while((line = reader.readLine()) != null)
+            {
+                if(line.contains("QUERY RESULT: ")) {
+                    line = line.substring(14,15) + ";";
+                    sb.append(line);
+                }
+                if(line.contains("TRACKID: ")){
+                    String[] lineParts = line.split(";");
+                    String track_id = lineParts[0].substring(9);
+                    String track_name = lineParts[1].substring(11);
+                    String track_dist = lineParts[2].substring(11);
+                    String track_time = lineParts[3].substring(11);
+                    String track_avg = lineParts[4].substring(14);
+                }
+            }
+            return sb.toString();
+        }catch(UnsupportedEncodingException e){
+            return new String("UEEException: " + e.getMessage());
+        }
+        catch(MalformedURLException e){
+            return new String("MUException: " + e.getMessage());
+        }catch(IOException e){
+            return new String("IOException: " + e.getStackTrace().toString());
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result){
+        System.out.println("Result: " + result);
+    }
+}
---
+++ app/src/main/java/com/example/alien/myapplication1/tracks/Track.java
@@ -0,0 +1,40 @@
+package com.example.alien.myapplication1.tracks;
+
+/**
+ * Created by Alien on 2015-04-29.
+ */
+public class Track {
+    private int _track_id;
+    private String _track_name;
+    private int _distance;
+    private String _time;
+    private double _average;
+
+    Track(int track_id, String track_name, int distance, String time, double average){
+        _track_id = track_id;
+        _track_name = track_name;
+        _distance = distance;
+        _time = time;
+        _average = average;
+    }
+
+    public int getTrackId(){
+        return _track_id;
+    }
+
+    public String getTrackName(){
+        return _track_name;
+    }
+
+    public int getDistance(){
+        return _distance;
+    }
+
+    public String getTime(){
+        return _time;
+    }
+
+    public double getAverage(){
+        return _average;
+    }
+}
